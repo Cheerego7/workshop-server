b4a1bbba3d24e5f39062c946bc9ca658426dce0c
cheerchen <meetcheerego@gmail.com>
2022-06-16 17:09:19 +0800

    feat: add route of the create room token

--- src/typings/globals.d.ts
+++ src/typings/globals.d.ts
@@ -1,7 +1,7 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         SERVER_PORT: string;
-        ALIBABA_ACCESS_KEY: string;
-        ALIBABA_SECRET_ACCESS_KEY: string;
+        NETLESS_ACCESS_KEY: string;
+        NETLESS_SECRET_ACCESS_KEY: string;
     }
 }
